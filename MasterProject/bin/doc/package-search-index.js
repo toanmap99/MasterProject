837bd29129111b5b5fb323be398472fb68f09b92
toanmap99 <72945571+toanmap99@users.noreply.github.com>
2025-07-16 22:20:52 -0700

    Delete MasterProject directory

--- MasterProject/bin/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.graymatter.demo"},{"l":"com.graymatter.demo.controller"},{"l":"com.graymatter.demo.model"},{"l":"com.graymatter.demo.repo"},{"l":"com.graymatter.demo.service"}];updateSearchResults();
